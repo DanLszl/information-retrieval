76b6ccdab2e032bdeb9beca3c5403846ef81356d
ximua00 <kishanarendra@gmail.com>
2019-01-20 15:55:55 +0100

    Click Models finished

--- rcm.py
+++ rcm.py
@@ -7,9 +7,9 @@ from numpy.random import binomial
 
 class RandomClickModel:
     def __init__(self, session_data, clicks_per_session):
-        self.rho = self.estimate_rho(session_data, clicks_per_session)
+        self.rho = self.estimate_parameters(session_data, clicks_per_session)
 
-    def estimate_rho(self, session_data, clicks_per_session):
+    def estimate_parameters(self, session_data, clicks_per_session):
         ndocs = 0
         nclicks = 0
         for id, docs in session_data.items():
@@ -18,13 +18,17 @@ class RandomClickModel:
         return nclicks / ndocs
 
     def simulate_clicks(self, interleaved_results, *args):
+        clicked_or_not_clicked = self.click_probability(interleaved_results)
+        return np.where(clicked_or_not_clicked)[0]
+
+    def click_probability(self, interleaved_results):
         clicked_or_not_clicked = binomial(
             1, self.rho, len(interleaved_results))
-        return np.where(clicked_or_not_clicked)[0]
+        return clicked_or_not_clicked
 
 
 def get_session_data_and_clicks_per_session():
-    log_file = "YandexRelPredChallenge.txt"
+    log_file = "test2.txt"
     session_data, clicks_per_session = parse_log_data(log_file)
     return session_data, clicks_per_session
 
